9ce40dbca991e53ac74d2555351a17cab3ccfc2e
foymikek <foymikek@gmail.com>
2021-09-26 14:36:57 -0600

    terrafrommanifest updated

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -1,11 +1,20 @@
 # Terraform Block
-
+terraform {
+  required_version = "~> v1.0.7"
+  required_providers {
+      aws = {
+      source = "hashicorp/aws"
+      version = "~>3.6"
+  }
+}
 
 # Provider Block
+provider "aws" {
+  
+}
 
 
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
 $HOME/.aws/credentials
 */
-
