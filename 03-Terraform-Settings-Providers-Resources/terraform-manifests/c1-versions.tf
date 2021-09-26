b4034686d450224eba3475d06d723f8edfb4178a
foymikek <foymikek@gmail.com>
2021-09-26 15:30:50 -0600

    provider block error

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -1,17 +1,20 @@
 # Terraform Block
 terraform {
-  required_version = "~> v1.0.7"
+  required_version = "~> v1.0"
   required_providers {
       aws = {
       source = "hashicorp/aws"
-      version = "~>3.6"
+      version = "~> 3.6"
   }
 }
 
+
 # Provider Block
 provider "aws" {
-  
+  region  = "us-east-1"
 }
+
+
 
 
 /*
