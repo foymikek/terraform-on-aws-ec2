31a5d5ade89cfda5de00ddfeb2ec3787995ab152
foymikek <foymikek@gmail.com>
2021-09-27 14:14:01 -0600

    successful terraform init

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -6,15 +6,21 @@ terraform {
       source = "hashicorp/aws"
       version = "~> 3.6"
   }
+ }
 }
 
 
 # Provider Block
 provider "aws" {
   region  = "us-east-1"
+  access_key = "value"
+  secret_key = "value"
 }
 
-
+resource "aws_instance" "ec2demo" {
+  ami           = "value"
+  instance_type = "value"
+}
 
 
 /*
