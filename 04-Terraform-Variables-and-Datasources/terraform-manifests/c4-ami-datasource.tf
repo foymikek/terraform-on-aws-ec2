7dfc2a6d04b5124eac65ef26c4a080780962d422
foymikek <foymikek@gmail.com>
2021-10-08 14:59:57 -0600

    add c4 c5

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf
@@ -1,5 +1,8 @@
 # Get latest AMI ID for Amazon Linux2 OS
 #AMI = Amazon Machine Image, template for creating AWS virtual server, EC2. 
+
+# data.aws_ami.amzlinux2.id
+
 data "aws_ami" "amzlinux2" {
   most_recent      = true
   owners           = ["amazon"]
@@ -8,12 +11,10 @@ data "aws_ami" "amzlinux2" {
     name   = "name"
     values = ["amzn2-ami-hvm-*-gp2"]
   }
-
   filter {
     name   = "root-device-type"
     values = ["ebs"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf
@@ -1,2 +1,14 @@
 # EC2 Instance
-
+resource "aws_instance" "myec2vm" {
+  ami = data.aws_ami.amzlinux2.id
+  instance_type = var.instance_type
+  user_data = file("${path.module}/app1-install.sh")
+  vpc_security_group_ids = [
+    aws_security_group.vpc-ssh.id,
+    aws_security_group.vpc-web.id
+  ]
+  key_name = var.variable_keypair
+  tags = {
+    "Name" = "EC2 Demo"
+  }
+}
