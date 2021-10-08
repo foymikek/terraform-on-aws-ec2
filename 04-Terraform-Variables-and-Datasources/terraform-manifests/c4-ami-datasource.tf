f181163ff0809ab1be2ed6aea8c7f160e6a716c0
foymikek <foymikek@gmail.com>
2021-10-08 13:18:05 -0600

    ami data source

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf
@@ -1,1 +1,21 @@
 # Get latest AMI ID for Amazon Linux2 OS
+#AMI = Amazon Machine Image, template for creating AWS virtual server, EC2. 
+data "aws_ami" "amzlinux2" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["myami-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
