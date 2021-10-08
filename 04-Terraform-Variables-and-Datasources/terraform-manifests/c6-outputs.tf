7e14fa8e297219d328aea73d9af8575bd71b80ed
foymikek <foymikek@gmail.com>
2021-10-08 15:03:14 -0600

    add outputs

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c6-outputs.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c6-outputs.tf
@@ -1,6 +1,13 @@
 # Terraform Output Values
 
 # EC2 Instance Public IP
-
+output "instance_public_ip" {
+  description = "ec2 instance public IP"
+  value = aws_instance.myec2vm.public_ip
+}
 
 # EC2 Instance Public DNS
+output "instance_public_dns" {
+  description = "ec2 instance public IP"
+  value = aws_instance.myec2vm.public_dns
+}
